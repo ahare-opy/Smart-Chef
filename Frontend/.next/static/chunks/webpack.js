6cf0258a5e1c565dd20f78d197fcd0d5caafa1e6
TanvirYeasinOpy <tanvir016opy@gmail.com>
2024-05-21 22:19:55 +0600

    Made changes

--- Frontend/.next/static/chunks/webpack.js
+++ Frontend/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "20c8743a97f00d5a"; }
+/******/ 		__webpack_require__.h = function() { return "b6bddfd5618ed590"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
